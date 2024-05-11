01472f5af7eb01dc0273d99d54478b7b023a3218
MarianoMonzi <arielmariano.monzon2002@gmail.com>
2024-05-11 14:59:07 -0300

    settins updated

--- lubricentro/settings.py
+++ lubricentro/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-1tm8$*3zbonok5q5y(w-@d#0yq2+nd!k5gqoj9r-fl1z2n@%i^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'web-production-7f80.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'web-production-7f80.up.railway.app']
 
 
 # Application definition
